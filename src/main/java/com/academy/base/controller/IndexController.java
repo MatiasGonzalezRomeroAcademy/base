bda681495300ff566c77d4589fdd020c53dc93f5
MatiasGonzalezRomero <maedgoro@gmail.com>
2020-11-08 14:18:42 -0300

    academy: adding index controller

---
+++ src/main/java/com/academy/base/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.academy.base.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping(value = "/")
+	public String welcomeAsHTML() {
+		return "index.html";
+	}
+}
